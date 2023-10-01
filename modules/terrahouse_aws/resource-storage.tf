82262f8801b91253fad9a2e215869fa056bf67b6
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-10-01 20:40:57 +0100

    more fixes

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -24,27 +24,27 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
-resource "aws_s3_object" "index_html" {
-  bucket       = aws_s3_bucket.website_bucket.bucket
-  key          = "index.html"
-  source       = "${path.root}${var.index_html_filepath}"
-  content_type = "text/html"
+# resource "aws_s3_object" "index_html" {
+#   bucket       = aws_s3_bucket.website_bucket.bucket
+#   key          = "index.html"
+#   source       = "${path.root}${var.index_html_filepath}"
+#   content_type = "text/html"
 
-  etag = filemd5("${path.root}${var.index_html_filepath}")
-  lifecycle {
-    replace_triggered_by = [terraform_data.content_version.output]
-    ignore_changes       = [etag]
-  }
-}
+#   etag = filemd5("${path.root}${var.index_html_filepath}")
+#   lifecycle {
+#     replace_triggered_by = [terraform_data.content_version.output]
+#     ignore_changes       = [etag]
+#   }
+# }
 
-resource "aws_s3_object" "error_html" {
-  bucket       = aws_s3_bucket.website_bucket.bucket
-  key          = "error.html"
-  source       = "${path.root}${var.error_html_filepath}"
-  content_type = "text/html"
+# resource "aws_s3_object" "error_html" {
+#   bucket       = aws_s3_bucket.website_bucket.bucket
+#   key          = "error.html"
+#   source       = "${path.root}${var.error_html_filepath}"
+#   content_type = "text/html"
 
-  etag = filemd5("${path.root}${var.error_html_filepath}")
-}
+#   etag = filemd5("${path.root}${var.error_html_filepath}")
+# }
 
 # resource "aws_s3_object" "pre_js" {
 #   bucket       = aws_s3_bucket.website_bucket.bucket
