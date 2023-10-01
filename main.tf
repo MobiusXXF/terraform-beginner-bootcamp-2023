4e5934db7a9124855ded2589eeb279df2f89edd8
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-10-01 20:39:38 +0100

    fix

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ terraform {
 
 
 module "terrahouse_aws" {
-  source              = "./modules/terrahouse_aws"
-  user_uuid           = var.user_uuid
-  bucket_name         = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
+  source      = "./modules/terrahouse_aws"
+  user_uuid   = var.user_uuid
+  bucket_name = var.bucket_name
+  # index_html_filepath = var.index_html_filepath
+  # error_html_filepath = var.error_html_filepath
   # pre_js_filepath     = var.pre_js_filepath
   # random_js_filepath  = var.random_js_filepath
   # styles_css_filepath = var.styles_css_filepath
