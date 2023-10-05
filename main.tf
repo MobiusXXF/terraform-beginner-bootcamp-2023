b5a4b170704dce57238c1c3b19b9e8229dc09a9a
Anthony Adesanwo <cosmoplasty@gmail.com>
2023-10-05 19:52:39 +0000

    #43 Implementing CRUD for terraform provider

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
   
@@ -34,3 +34,20 @@ provider "terratowns" {
 #   content_version     = var.content_version
 # }
 
+resource "terratowns_home" "soul_house" {
+  name = "Dive In With The Soulquarians"
+  description = <<DESCRIPTION
+A revolutionary rotating hip-hop/soul group, founded by great producers, 
+the likes of J-Dilla, Questlove, James Poyner and D'Angelo.
+The name of the collective is derived from an astrology sign Aquarius,
+which is the shared birth sign of the founding members of the collective.
+Made up of prominent black artists of the 90s and early 200s, such as
+Q-Tip, Erykah Badu, Blackthought, Yasiin Bey, Talid Kweli, Common, Pino Palladino and Roy Hargrove.
+
+(Did you know sound travels faster underwater?)
+DESCRIPTION
+  # domain_name = module.terrahouse_aws.cloudfront_url 
+  domain_name = "454389.cloudfront.net"  
+  town = "melomaniac-mansion"
+  content_version = 1
+}
