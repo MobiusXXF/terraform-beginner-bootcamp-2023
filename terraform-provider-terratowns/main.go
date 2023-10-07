f891339104b142224fb1a445a4e6a61ad2c8f1b1
Anthony Adesanwo <104108778+MobiusXXF@users.noreply.github.com>
2023-10-07 02:18:35 +0100

    #48 terratowns test (#49)
    * #48 testing deployment into missingo town, refactoring variables after using terraform mock server
    
    * Add files via upload

--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -127,7 +127,7 @@ func Resource() *schema.Resource {
 	log.Print("Resource:start")
 	return resource
 }
-https://mobiusxxf-terraformbegi-8nixdz9oicf.ws-eu105.gitpod.io/
+
 func resourceHouseCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	log.Print("resourceHouseCreate:start")
 	var diags diag.Diagnostics
@@ -146,7 +146,7 @@ func resourceHouseCreate(ctx context.Context, d *schema.ResourceData, m interfac
 		return diag.FromErr(err)
 	}
 
-	url :=  config.Endpoint+"/u/"+config.UserUuid+"/homes"
+	url := config.Endpoint+"/u/"+config.UserUuid+"/homes"
 	log.Print("URL: "+ url)
 	// Construct the HTTP Request
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
